9f0698b686d4553e344ea6edda7421c824a32b36
Marasmo <pierovitorc@gmail.com>
2026-06-28 14:22:59 -0500

    feat: integracion kanban dashboard, triggers ventas, busqueda full-text

--- src/lib/supabaseDashboard.js
+++
@@ -1,8 +0,0 @@
-// src/lib/supabaseDashboard.js
-// Cliente separado para el Dashboard Kanban de Óptica Juliaca
-import { createClient } from '@supabase/supabase-js'
-
-const DASHBOARD_URL  = 'https://clejkcmkowkzasztouwn.supabase.co/rest/v1/'
-const DASHBOARD_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsZWprY21rb3dremFzenRvdXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MTc1MzMsImV4cCI6MjA5NTQ5MzUzM30.LzLQL7wGkp8px48YvGdBqDImOU82ep1H07YKwA0qtMY'
-
-export const supabaseDashboard = createClient(DASHBOARD_URL, DASHBOARD_ANON)
